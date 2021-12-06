4920060814ddf5d4c1204991930a25284f3ea41e
Madalina Varasteanu <madalinavarasteanu@rocketmortgage.com>
2021-12-06 00:23:06 -0500

    initial commit

---
+++ MovieRegistration/MovieRegistration/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+
+       public IActionResult Result(Movie m)
+        {
+            return View(m);
+        }
+            
+
+       
+    }
+}
---
+++ MovieRegistration/MovieRegistration/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+
+    }
+
+   public enum Genre
+    {
+        Action,
+        Animation,
+        Comedy,
+        Drama,
+        Thriller,
+    }
+}
